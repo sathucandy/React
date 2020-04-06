cb2ba91f6f8b50b06139dec5cba2fc7e9e1705d5
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-04-06 09:23:59 +0530

    Adding dynamic content

--- 1-monster-rolodex/src/App.js
+++ 1-monster-rolodex/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monster: [
+        {
+          name: "Frankenstien",
+          id: "asc1",
+        },
+        {
+          name: "Dracula",
+          id: "asc2",
+        },
+        {
+          name: "Zombie",
+          id: "asc3",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monster.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
